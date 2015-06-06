eb388b22b6226dd9c0d3ea361c9092e4a461925a
baptiste <baptiste.auguie@gmail.com>
2015-06-06 20:55:55 +1200

    vectorised interface to complex functions

--- src/callFaddeeva.cpp
+++ src/callFaddeeva.cpp
@@ -5,11 +5,69 @@ using namespace Rcpp;
 #include "Faddeeva.h"
 
 // [[Rcpp::export]]
-double Dawson(double x) { // special case for real x
-  return Faddeeva::Dawson(x);
+std::vector< std::complex<double> > erfcx(ComplexVector z_, double relerr=0) { 
+  std::vector< std::complex<double> > z = as< std::vector< std::complex<double> > >(z_);
+  int N = z_.size(), i;
+  std::vector< std::complex<double> > result(N);
+  for(int i=0; i<N; i++) {
+    result[i] = Faddeeva::erfcx(z[i], relerr);
+  }
+  return result;
 }
 
 // [[Rcpp::export]]
-std::complex<double> Faddeeva_w(std::complex<double> z, double relerr) { 
-  return Faddeeva::w(z, relerr);
+std::vector< std::complex<double> > erf(ComplexVector z_, double relerr=0) { 
+  std::vector< std::complex<double> > z = as< std::vector< std::complex<double> > >(z_);
+  int N = z_.size(), i;
+  std::vector< std::complex<double> > result(N);
+  for(int i=0; i<N; i++) {
+    result[i] = Faddeeva::erf(z[i], relerr);
+  }
+  return result;
 }
+
+// [[Rcpp::export]]
+std::vector< std::complex<double> > erfi(ComplexVector z_, double relerr=0) { 
+  std::vector< std::complex<double> > z = as< std::vector< std::complex<double> > >(z_);
+  int N = z_.size(), i;
+  std::vector< std::complex<double> > result(N);
+  for(int i=0; i<N; i++) {
+    result[i] = Faddeeva::erfi(z[i], relerr);
+  }
+  return result;
+}
+
+// [[Rcpp::export]]
+std::vector< std::complex<double> > erfc(ComplexVector z_, double relerr=0) { 
+  std::vector< std::complex<double> > z = as< std::vector< std::complex<double> > >(z_);
+  int N = z_.size(), i;
+  std::vector< std::complex<double> > result(N);
+  for(int i=0; i<N; i++) {
+    result[i] = Faddeeva::erfc(z[i], relerr);
+  }
+  return result;
+}
+
+// [[Rcpp::export]]
+std::vector< std::complex<double> > Dawson(ComplexVector z_, double relerr=0) { 
+  std::vector< std::complex<double> > z = as< std::vector< std::complex<double> > >(z_);
+  int N = z_.size(), i;
+  std::vector< std::complex<double> > result(N);
+  for(int i=0; i<N; i++) {
+    result[i] = Faddeeva::Dawson(z[i], relerr);
+  }
+  return result;
+}
+
+// [[Rcpp::export]]
+std::vector< std::complex<double> > Faddeeva_w(ComplexVector z_, double relerr=0) { 
+  std::vector< std::complex<double> > z = as< std::vector< std::complex<double> > >(z_);
+  int N = z_.size(), i;
+  std::vector< std::complex<double> > result(N);
+  for(int i=0; i<N; i++) {
+    result[i] = Faddeeva::w(z[i], relerr);
+  }
+  return result;
+}
+
+
--- tests/minimal.R
+++ tests/minimal.R
@@ -2,4 +2,4 @@ library(RcppFaddeeva)
 ?RcppFaddeeva
 
 Dawson(4.2)
-Faddeeva_w(1+1i, 0.1)
+Faddeeva_w(1:10+1i, 0.1)
