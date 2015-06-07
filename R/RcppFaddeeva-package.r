6c8368e674020e891b42c0e4986f812c8c6289d3
baptiste <baptiste.auguie@gmail.com>
2015-06-07 15:31:13 +1200

    not so many ees

--- R/RcppFaddeeva-package.r
+++ R/RcppFaddeeva-package.r
@@ -2,11 +2,11 @@
 ##'
 ##' Access to error functions for arbitrary complex arguments is provided via the Faddeeva package by Steven G. Johnson.
 ##' 
-##' @name RcppFaddeeeva-package
+##' @name RcppFaddeeva-package
 ##' @docType package
 ##' @useDynLib RcppFaddeeva
 ##' @importFrom Rcpp evalCpp
-##' @title RcppFaddeeeva
+##' @title RcppFaddeeva
 ##' @references
 ##' The Faddeeva Package wiki page details the algorithms implemented by Steve G. Johnson, 
 ##' \url{http://ab-initio.mit.edu/wiki/index.php/Faddeeva_Package}
